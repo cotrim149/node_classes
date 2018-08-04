23d862b0eb32d97abecd7156c7bc51777da673f7
Victor Cotrim <cotrim149@gmail.com>
2018-08-04 18:14:26 -0300

    Adding uniq function to test lodash

--- 02-notes-node/app.js
+++ 02-notes-node/app.js
@@ -2,10 +2,17 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var sumResult = notes.sum(9,-2);
-console.log(sumResult);
+var filteredArray = _.uniq(['Cotrim', 1, 'Cotrim', 1,2,3,2,4]);
+console.log(filteredArray);
+
+// var sumResult = notes.sum(9,-2);
+// console.log(sumResult);
+
+// console.log(_.isString(true));
+// console.log(_.isString('Cotrim'));
 
 // var user = os.userInfo();
 // var userName = user.username;
