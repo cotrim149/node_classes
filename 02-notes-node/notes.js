bb445dbfd5fa70a1cc1f33738490ae12cf715678
Victor Cotrim <cotrim149@gmail.com>
2018-08-05 18:14:57 -0300

    Removing commented lines and greetings file

--- 02-notes-node/notes.js
+++ 02-notes-node/notes.js
@@ -4,8 +4,3 @@ module.exports.addNote = () => {
   console.log('Add note');
   return 'New Note';
 };
-
-module.exports.sum = (num1 , num2) => {
-  console.log(`Sum of ${num1} + ${num2}`);
-  return num1 + num2;
-}
