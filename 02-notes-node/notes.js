fe3eeedc01a1ad770db86d5511cdbdf9ee30dd6a
Victor Cotrim <cotrim149@gmail.com>
2018-08-08 21:08:12 -0300

    Adding implementation to AddNote method

--- 02-notes-node/notes.js
+++ 02-notes-node/notes.js
@@ -1,7 +1,32 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  console.log(note);
+  try{
+    // fetching data from notes-data.json
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+    console.log(notes);
+  } catch (error) {
+  
+  }
+  
+  let duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json',JSON.stringify(notes));  
+  }
+
 };
 
 var getAll = () => {
