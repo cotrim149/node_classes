70226d423d6de718248d35fcbcac9b7e3c926cfa
Victor Cotrim <cotrim149@gmail.com>
2018-08-11 13:59:44 -0300

    Adding help and description functions to commands

--- 02-notes-node/app.js
+++ 02-notes-node/app.js
@@ -3,7 +3,28 @@ const _ = require('lodash');
 const yargs = require('yargs');
 const notes = require('./notes.js');
 
-var argv = yargs.argv;
+var addStringOptions = (describe, demand, alias) => {
+  return {
+    describe: describe,
+    demand: demand,
+    alias: alias
+  };
+};
+
+var argv = yargs
+  .command('add', 'Add a new note', {
+    title: addStringOptions('Title of a note', true, 't'),
+    body: addStringOptions('Body of a note', true, 'b')
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: addStringOptions('Title of a note', true, 't'),
+  })
+  .command('remove', 'Remove a specific note', {
+    title: addStringOptions('Title of a note', true, 't')
+  })
+  .help()
+  .argv;
 var command = argv._[0];
 
 switch(command) {
