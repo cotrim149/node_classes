5e9c7f55ac1d2146821505f14501c5baf454015c
Victor Cotrim <cotrim149@gmail.com>
2018-08-11 20:04:26 -0300

    Adding google geocoder request

--- 03-weather-app/app.js
+++ 03-weather-app/app.js
@@ -4,5 +4,8 @@ request({
   url: 'http://maps.googleapis.com/maps/api/geocode/json?address=Rua%204A%20ch%C3%A1cara%20112%20lote%2005',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  const latitude = body.results[0].geometry.location.lat;
+  const longitude = body.results[0].geometry.location.lng;
+  console.log(`Coordinate of address\n lat: ${latitude} lng: ${longitude}`);
 });
