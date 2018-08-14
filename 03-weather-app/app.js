8fc2775508be7364672db57550eb3107392b9adf
Victor Cotrim <cotrim149@gmail.com>
2018-08-13 22:40:10 -0300

    Adding request npm package and adding address string dinamically to get lat and long

--- 03-weather-app/app.js
+++ 03-weather-app/app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for.',
+      string: true,
+    }
+  })
+  .help()
+  .alias('help','h')
+  .argv;
+
+  var encodedAddress = encodeURIComponent(argv.address);
+  console.log(encodedAddress);
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=Rua%204A%20ch%C3%A1cara%20112%20lote%2005',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
