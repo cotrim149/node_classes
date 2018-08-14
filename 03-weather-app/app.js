857ac4150128291d70a39d00ecb770e0638fb292
Victor Cotrim <cotrim149@gmail.com>
2018-08-13 23:27:23 -0300

    Refactoring fetch request of app.js to geocode module

--- 03-weather-app/app.js
+++ 03-weather-app/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -14,24 +14,10 @@ const argv = yargs
   .alias('help','h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-
-  if (error) {
-    console.log('Unable to connect with google service');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address');
-  } else if (body.status === 'INVALID_REQUEST') {
-    console.log('Address is invalid');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    const latitude = body.results[0].geometry.location.lat;
-    const longitude = body.results[0].geometry.location.lng;
-    console.log(`Coordinate of address\n lat: ${latitude} lng: ${longitude}`)
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
-
 });
---
+++ 03-weather-app/geocode/geocode.js
@@ -0,0 +1,34 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+
+  var encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body) => {
+
+    if (error) {
+      callback('Unable to connect with google service', undefined);
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback('Unable to find that address', undefined);
+    } else if (body.status === 'INVALID_REQUEST') {
+      callback('Address is invalid', undefined);
+    } else if (body.status === 'OK') {
+      const latitude = body.results[0].geometry.location.lat;
+      const longitude = body.results[0].geometry.location.lng;
+
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: latitude,
+        longitude: longitude
+      })
+    }
+
+  });
+};
+
+module.exports = {
+  geocodeAddress
+}
