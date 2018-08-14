9c5ca00712e0bb8f9df698d2f30228386671cb82
Victor Cotrim <cotrim149@gmail.com>
2018-08-14 20:52:51 -0300

    Neasted promise

--- 03-weather-app/playground/promises.js
+++ 03-weather-app/playground/promises.js
@@ -1,12 +1,38 @@
-var somePromisse = new Promise(((resolve, reject) => {
-  setTimeout(() => {
-    // resolve('Hey. It worked');
-    reject('Unable to fulfill the promise.');
-  }, 2500);
-}));
 
-somePromisse.then((message) => {
-  console.log(message);
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number'){
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+
+  });
+}
+
+asyncAdd(5 , 7).then((result) => {
+  console.log(`Result: ${result}`);
+  return asyncAdd(result, 33);
+}, (errorMessage) => {
+  console.log(errorMessage);
+}).then((result) => {
+  console.log('Expect result be 45. Result: ',result);
 }, (errorMessage) => {
   console.log(errorMessage);
 });
+
+// var somePromisse = new Promise(((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve('Hey. It worked');
+//     reject('Unable to fulfill the promise.');
+//   }, 2500);
+// }));
+
+// somePromisse.then((message) => {
+//   console.log(message);
+// }, (errorMessage) => {
+//   console.log(errorMessage);
+// });
